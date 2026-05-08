81977393ca0762cd1e4ce0bac68953285bc07c03
Icaro Fracassi <icarofracassi@gmail.com>
2026-05-08 19:36:33 -0300

    Removed unused imports

--- web/src/components/ui/calendar.tsx
+++ web/src/components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { DayPicker } from "react-day-picker";
 import { cn } from "@/lib/utils";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import "react-day-picker/style.css";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
